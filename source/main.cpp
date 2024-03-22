6dc09b650e84636c43d72d1dc3250928c609092b
Tedd OKANO <okano.akifumi@gmail.com>
2024-03-23 07:28:57 +0900

    modified to use v0.9.0 libralry

--- source/main.cpp
+++ source/main.cpp
@@ -8,8 +8,7 @@
 #include	"r01lib.h"
 r01lib_start;	/* *** place this word before making instance of r01lib classes *** */
 
-I2C		i2c;
-
+I2C			i2c( I2C_SDA, I2C_SCL );	//	SDA, SCL
 DigitalOut	rst( D9, 1 );
 DigitalOut	oe(  D8, 0 );
 
